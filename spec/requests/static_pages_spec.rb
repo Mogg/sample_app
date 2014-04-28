cb5ebdfe0b81668568d610d314212bda61807b25
Hikari Shiozaki <mogg3141@gmail.com>
2014-04-29 03:10:36 +0900

    Add tests of the contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -17,12 +17,12 @@ describe "Static pages" do
 
     describe "Help page" do
 
-        it "should have the h1 'Help'" do
+        it "should have the content 'Help'" do
             visit '/static_pages/help'
             expect(page).to have_content('Help')
         end
 
-        it "should have the title 'Help'" do
+        it "should have the right title 'Help'" do
             visit '/static_pages/help'
             expect(page).to have_title("Ruby on Rails Tutorial Smaple App | Help")
         end
@@ -30,14 +30,27 @@ describe "Static pages" do
 
     describe "About page" do
 
-        it "should have the h1 'About Us'" do 
+        it "should have the content 'About Us'" do 
             visit '/static_pages/about'
             expect(page).to have_content('About Us')
         end
 
-        it "should have the title 'About Us'" do
+        it "should have the right title 'About Us'" do
             visit '/static_pages/about'
             expect(page).to have_title("Ruby on Rails Tutorial Smaple App | About Us")
+        end
+    end
+
+    describe "Contact page" do
+
+        it "should have the content 'Contact'" do 
+            visit '/static_pages/contact'
+            expect(page).to have_content('Contact')
+        end
+
+        it "should have the title 'Contact'" do
+            visit '/static_pages/contact'
+            expect(page).to have_title("Ruby on Rails Tutorial Smaple App | Contact")
         end
     end
 
